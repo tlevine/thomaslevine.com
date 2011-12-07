56d70e6fd85aa35ccd677972fa808321f07635be
Thomas Levine <thomas.levine@gmail.com>
2011-12-07 03:40:50 -0500

    !!!

--- www/js/script.js
+++ www/js/script.js
@@ -1,26 +1,6 @@
-/* Author: 
+/* Author: Thomas Levine
 
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var username='perluette'
+window.chainsaw=[username,'thomaslevine.com'].join('@');
+document.getElementById("e-post").innerHTML=chainsaw;
