9031b48d881c901991a4696d85367967ac96c6da
Thomas Levine <thomas.levine@gmail.com>
2011-12-07 04:27:38 -0500

    ?

--- api/email_address/models.py
+++ api/email_address/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class EmailAddress(models.Model):
   username=models.CharField(max_length=42)
-  date=models.DateTimeField(null=True)
+  date=models.DateTimeField(auto_now=True)
   REMOTE_ADDR=models.IPAddressField(blank=True)
+  used=models.BooleanField(default=False)
   def __uni__(self):
     return self.address
--- api/email_address/views.py
+++ api/email_address/views.py
@@ -1,15 +1,14 @@
-from datetime import datetime
 from django.shortcuts import render_to_response
 from models import EmailAddress
 
 def make_email_address(request):
   try:
-    e=EmailAddress.objects.filter(date__isnull=True)[0]
+    e=EmailAddress.objects.filter(used=False)[0]
   except IndexError:
     username='ligature'
   else:
     e.REMOTE_ADDR=request.META['REMOTE_ADDR']
-    e.date=datetime.now()
+    e.used=True
     e.save()
     username=e.username
   return render_to_response('email.js',{"username":username},mimetype="text/javascript")
