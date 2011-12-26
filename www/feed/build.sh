676df3ac58a24a80eb157bf259adff1c48aad810
Thomas Levine <thomas.levine@gmail.com>
2011-12-26 01:59:36 -0500

    yay

--- www/feed/build.sh
+++ www/feed/build.sh
@@ -2,13 +2,22 @@
 FORMATS='ntriples atom rss-1.0 dot'
 PUBLISH_DIR=publish
 
-for format in $FORMATS; do
-  if [[ $format = rss-1.0 ]];
-    then extension=rss
-  elif [[ $format = ntriples ]];
-    then extension=rdf
-  else
-    extension=$format
-  fi
-  rapper -i turtle -o $format index.turtle > $PUBLISH_DIR/index.$extension
+#List the files
+files=index.ttl
+files+=`ls -d posts/*` || sleep 0
+files+=`ls -d tags/*`  || sleep 0
+
+#Process the files
+for file in $files; do
+  for format in $FORMATS; do
+    if [[ $format = rss-1.0 ]];
+      then extension=rss
+    elif [[ $format = ntriples ]];
+      then extension=rdf
+    else
+      extension=$format
+    fi
+    filebase="`basename $file .ttl`"
+    rapper -i turtle -o $format $file > $PUBLISH_DIR/$filebase.$extension
+  done
 done
