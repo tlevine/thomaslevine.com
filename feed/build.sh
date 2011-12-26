fb47b815e505f538236117551cb8127d3d569890
Thomas Levine <thomas.levine@gmail.com>
2011-12-26 14:22:44 -0500

    aoeu

--- feed/build.sh
+++ feed/build.sh
@@ -3,38 +3,18 @@ FORMATS='atom rss-1.0 rdfxml'
 INTERMEDIATE_DIR=../intermediate
 PUBLISH_DIR=../publish
 
-#Concatenate the files
-for dir in $INTERMEDIATE_DIR $PUBLISH_DIR; do 
-  mkdir -p $dir/posts $dir/tags
-  for subdir in . posts tags; do
-    mkdir -p $dir/$subdir
-    cp templates/header.ttl $dir/$subdir/index.ttl
-    cat $subdir/index.ttl >> $dir/$subdir/index.ttl
-    cat $subdir/*/index.ttl >> $dir/$subdir/index.ttl
-  done
-done
+#Concatenate
+mkdir -p $INTERMEDIATE_DIR/posts
+cp posts/index.ttl $INTERMEDIATE_DIR/posts/index.ttl
+sed '/@prefix/d' posts/*/index.ttl > $INTERMEDIATE_DIR/posts/index.ttl
 
-#Leaf rdfs
-for file in `ls -d {tags,posts}/*/index.ttl`; do
-  filedir="`echo $file|sed 's/index\.ttl$//'`"
-  mkdir -p $INTERMEDIATE_DIR/$filedir $PUBLISH_DIR/$filedir
+#Convert to triples
+rapper -i turtle -o ntriples $INTERMEDIATE_DIR/posts/index.ttl > $INTERMEDIATE_DIR/posts/index.nt
 
-  filebase="`echo $file|sed 's/\.ttl$//'`"
-  cp templates/header.ttl $INTERMEDIATE_DIR/$filebase.ttl
-  cat $file >> $INTERMEDIATE_DIR/$filebase.ttl
-  rapper -i turtle -o rdfxml $INTERMEDIATE_DIR/$filebase.ttl > $PUBLISH_DIR/$filebase.rdf
-done
+#Add RSS and Atom entities
 
-#Process the syndication feed files
-for format in $FORMATS; do
-  if [[ $format = rss-1.0 ]];
-    then extension=rss
-  elif [[ $format = rdfxml ]];
-    then extension=rdf
-  else
-    extension=$format
-  fi
-  rapper -i turtle -o $format $INTERMEDIATE_DIR/tags/index.ttl > $PUBLISH_DIR/tags/index.$extension
-  rapper -i turtle -o $format $INTERMEDIATE_DIR/posts/index.ttl > $PUBLISH_DIR/posts/index.$extension
-#  cp $PUBLISH_DIR/posts/index.$extension $PUBLISH_DIR/feed.$extension
-done
+#Convert to the three output formats
+#RSS and Atom are aggregated, so they use the intermediate directory.
+rapper -i turtle -o rdfxml posts/index.ttl > $PUBLISH_DIR/posts/index.rdf
+rapper -i ntriples -o rss-1.0 $INTERMEDIATE_DIR/posts/index.nt > $PUBLISH_DIR/posts/index.rss
+rapper -i ntriples -o atom $INTERMEDIATE_DIR/posts/index.nt > $PUBLISH_DIR/posts/index.atom
