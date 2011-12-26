cb134cde0387d823fd896f22a2d71be048e4eab1
Thomas Levine <thomas.levine@gmail.com>
2011-12-26 00:25:24 -0500

    fade in one at a time

--- www/js/script.js
+++ www/js/script.js
@@ -4,5 +4,7 @@
 $(function(){
   var chainsaw=['perluette','thomaslevine.com'].join('@');
   document.getElementById("e-post").innerHTML=chainsaw;
-  $('.not-ie #card').hide().css('visibility','inherit').fadeIn('slow');
+  $('.not-ie .card').each(function(i){
+    $(this).delay(1000*i).hide().css('visibility','inherit').fadeIn('slow');
+  });
 });
