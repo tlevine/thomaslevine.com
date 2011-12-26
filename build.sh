0c45e65b14cf4a19f1a5e5fc90288b531451b29c
Thomas Levine <thomas.levine@gmail.com>
2011-12-26 18:53:03 -0500

    aoeu

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#Build the feed
-rapper -i turtle -o rdfxml feed/index.ttl > publish/feed/index.rdf
-
 #Build the website
 cd www/build
 ant build
 cd -
+
+#Build the feed
+mkdir -p www/publish/feed
+rapper -i turtle -o rdfxml feed/index.ttl > www/publish/feed/index.rdf
