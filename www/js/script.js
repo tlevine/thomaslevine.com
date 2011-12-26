8b4d9c9664f666ea8392229baddce6ebc34dd32b
Thomas Levine <thomas.levine@gmail.com>
2011-12-25 23:33:16 -0500

    mobile first

--- www/js/script.js
+++ www/js/script.js
@@ -4,13 +4,5 @@
 $(function(){
   var chainsaw=['perluette','thomaslevine.com'].join('@');
   document.getElementById("e-post").innerHTML=chainsaw;
-  $('.social a').click(function(){
-    var thelink=$(this).attr('href');
-    _gaq.push(['_trackEvent', 'Social', 'Clickthrough',thelink]);
-  });
-  $('div.name,div.e-post,div.phone,div.logo').click(function(){
-    var theclass=$(this).attr('class');
-    _gaq.push(['_trackEvent', 'Static', 'Click',theclass]);
-  });
-  $('#card').hide().css('visibility','inherit').fadeIn('slow');
+  $('.not-ie #card').hide().css('visibility','inherit').fadeIn('slow');
 });
