9de751563da0f13955f3fd31e8c221b74be79f70
Thomas Levine <thomas.levine@gmail.com>
2011-12-26 16:12:56 -0500

    okay i came up with something i might use

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #Build the feed
-cd feed
-./build.sh
-cd -
+rapper -i turtle -o rdfxml feed.ttl > publish/feed.rdf
+rapper -i turtle -o atom feed.ttl > publish/feed.atom
+rapper -i turtle -o rss-1.0 feed.ttl > publish/feed.rss
+
+#Build the tags
+#rapper -i turtle -o rdfxml tags.ttl > publish/tags.rdf
 
 #Build the website
 cd www/build
 ant build
 cd -
-
-#Convert the RDF for the website
-rapper -i turtle -o ntriples www/index.ttl > publish/index.rdf
