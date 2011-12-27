d9ae8a1144a8d81b80c516a7ac365b2273eb9d73
Thomas Levine <thomas.levine@gmail.com>
2011-12-26 19:04:59 -0500

    rename

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ cd -
 
 #Build the feed
 mkdir -p www/publish/feed
-rapper -i turtle -o rdfxml feed/index.ttl > www/publish/feed/index.rdf
+rapper -i turtle -o rdfxml feed/ttl > www/publish/feed/rdf
+rapper -i turtle -o rss-1.0 feed/ttl > www/publish/feed/rss
