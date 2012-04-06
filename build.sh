84478dabbca2bba02871c8ba7b6ebab73af7da1f
Thomas Levine <thomas.levine@gmail.com>
2012-04-06 19:20:54 -0400

    aoeu

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ ant build
 cd -
 
 #Build the feed
-mkdir -p www/publish/feed
-rapper -i turtle -o rdfxml feed/ttl > www/publish/feed/rdf
-rapper -i turtle -o rss-1.0 feed/ttl > www/publish/feed/rss
+#mkdir -p www/publish/feed
+#rapper -i turtle -o rdfxml feed/ttl > www/publish/feed/rdf
+#rapper -i turtle -o rss-1.0 feed/ttl > www/publish/feed/rss
--- www/js/script.js
+++ www/js/script.js
@@ -2,7 +2,7 @@
 
 */
 $(function(){
-  var chainsaw=['perluette','thomaslevine.com'].join('@');
+  var chainsaw=['occurrence','thomaslevine.com'].join('@');
   document.getElementById("e-post").innerHTML=chainsaw;
   $('.not-ie .card').each(function(i){
     $(this).delay(100*i).animate({opacity:1},'1000');
