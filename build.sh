40f94c6e70d569f97dcb19300a4592726d60e7eb
Thomas Levine <thomas.levine@gmail.com>
2012-04-07 10:24:34 -0400

    build script

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#Build the website
+# Delete old version
+rm -R www/publish
+
+# Build the website
 cd www/build
 ant build
 cd -
 
-#Build the feed
-#mkdir -p www/publish/feed
-#rapper -i turtle -o rdfxml feed/ttl > www/publish/feed/rdf
-#rapper -i turtle -o rss-1.0 feed/ttl > www/publish/feed/rss
+# Upload
+scp -r www/publish/* www-data@thomaslevine.com:/srv/www/thomaslevine.com/www
