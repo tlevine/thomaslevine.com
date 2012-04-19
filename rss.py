e4b75349215a12e13d26f0e2dfd26dceab0523cb
Thomas Levine <thomas.levine@gmail.com>
2012-04-18 23:18:48 -0400

    blog posts

--- rss.py
+++ rss.py
@@ -4,83 +4,129 @@
 import datetime
 import PyRSS2Gen
 import os
+import shutil
 import misaka
 from cgi import escape
 
 DOMAIN = "http://thomaslevine.com"
 RSS = DOMAIN + "/rss"
-BLOG_ROOT = DOMAIN + "/!/"
+BLOG_DIR = '!'
+BLOG_ROOT = DOMAIN + '/' + BLOG_DIR + '/'
 
-# Find all markdown files
-paths = []
-for dirname, dirnames, filenames in os.walk('blog'):
-    for filename in filenames:
-        # Select the markdown files.
-        if filename[-3:] == '.md':
-            paths.append(os.path.join(dirname, filename))
 
-# Make items out of them
-items = []
-for path in paths:
-    f = open(path, 'r')
+def getpaths(sourcedir = 'blog'):
+    "Find all markdown files"
+    mdpaths = []
+    otherpaths = []
+    for dirname, dirnames, filenames in os.walk(sourcedir):
+        for filename in filenames:
+            # Select the markdown files.
+            if filename[-3:] == '.md':
+                mdpaths.append(os.path.join(dirname, filename))
+            elif filename == 'rss':
+                raise ValueError('The file name "rss" is reserved for the rss feed.')
+            else:
+                otherpaths.append(os.path.join(dirname, filename))
 
-    # Title
-    title = f.readline()
+    # Don't do anything with otherpaths yet.
+    return mdpaths
 
-    # h1
-    if set('=') != set(f.readline()[:-1]):
-        raise ValueError("The second line of %s contains characters other than equal-signs." % path)
+def getrss(paths):
+    "Make rss items out of paths"
+    items = []
+    for path in paths:
+        f = open(path, 'r')
 
-    # Date
-    dateline = f.readline()[:-1]
-    if dateline.lower().strip() == 'draft':
-        continue
+        # Title
+        title = f.readline()
+
+        # h1
+        if set('=') != set(f.readline()[:-1]):
+            raise ValueError("The second line of %s contains characters other than equal-signs." % path)
+
+        # Date
+        dateline = f.readline()[:-1]
+        if dateline.lower().strip() == 'draft':
+            continue
+        try:
+            pubDate = datetime.datetime.strptime(dateline, '%B %d, %Y')
+        except ValueError:
+            params = (path, datetime.date.today().strftime('%B %d, %Y'))
+            raise ValueError('The third line of %s should be either the word "Draft" or a date in this format: %s.' % params)
+
+        # Optional categories line
+        categoriesline = f.readline().strip()
+        if len(categoriesline) >= 12 and categoriesline[0:12].lower() == 'categories: ':
+            categories = [category.strip() for category in categoriesline[12:].split(',')]
+            emptyline = f.readline().strip()
+        elif '' == categoriesline:
+            categories = []
+            emptyline = categoriesline
+        else:
+            raise ValueError("The fourth line of %s should have a list of categories or be empty." % path)
+         
+        # Empty line
+        if '' != emptyline:
+            raise ValueError("There should be an empty line between the header and body inside %s." % path)
+
+        # Description
+        try:
+            description = misaka.html(f.read())
+        except:
+            print("Error parsing %s" % path)
+            raise
+
+        # Close the file
+        f.close()
+
+        # Construct the link
+        link = BLOG_ROOT + path
+
+        # Append the item
+        items.append(PyRSS2Gen.RSSItem(
+             title = escape(title),
+             link = link,
+             guid =  PyRSS2Gen.Guid(link),
+             description = escape(description),
+             pubDate = pubDate,
+             categories = categories
+        ))
+
+    rss = PyRSS2Gen.RSS2(
+        title = "Thomas Levine",
+        link = RSS,
+        description = "Thomas Levine",
+        lastBuildDate = datetime.datetime.utcnow(),
+        items = items
+    )
+    return rss
+
+
+def cleanup(outdir = BLOG_DIR):
+    "Recreate the directory to which the blog will be published."
+
     try:
-        pubDate = datetime.datetime.strptime(dateline, '%B %d, %Y')
-    except ValueError:
-        params = (path, datetime.date.today().strftime('%B %d, %Y'))
-        raise ValueError('The third line of %s should be either the word "Draft" or a date in this format: %s.' % params)
+        shutil.rmtree(outdir)
+    except OSError:
+        pass #Directory doesn't exist
 
-    # Optional categories line
-    categoriesline = f.readline().strip()
-    if len(categoriesline) >= 12 and categoriesline[0:12].lower() == 'categories: ':
-        categories = [category.strip() for category in categoriesline[12:].split(',')]
-        emptyline = f.readline().strip()
-    elif '' == categoriesline:
-        categories = []
-        emptyline = categoriesline
-    else:
-        raise ValueError("The fourth line of %s should have a list of categories or be empty." % path)
-     
-    # Empty line
-    if '' != emptyline:
-        raise ValueError("There should be an empty line between the header and body inside %s." % path)
+    os.mkdir(outdir)
 
-    # Description
-    description = misaka.html(f.read())
+def main():
+    # Create the out directory
+    cleanup()
 
-    # Close the file
-    f.close()
+    # Get markdown files
+    paths = getpaths()
 
-    # Construct the link
-    link = BLOG_ROOT + path
+    # Copy them to the out directory
+    for inpath in paths:
+        outpath_tuple = (BLOG_DIR,) + os.path.split(inpath)[1:]
+        outpath = os.path.join(*outpath_tuple)
+        shutil.copy(inpath, outpath)
 
-    # Append the item
-    items.append(PyRSS2Gen.RSSItem(
-         title = escape(title),
-         link = link,
-         guid =  PyRSS2Gen.Guid(link),
-         description = escape(description),
-         pubDate = pubDate,
-         categories = categories
-    ))
+    # Make rss
+    rss = getrss(paths)
+    rss.write_xml(open(os.path.join('!', 'rss'), "w"))
 
-rss = PyRSS2Gen.RSS2(
-    title = "Thomas Levine",
-    link = RSS,
-    description = "Thomas Levine",
-    lastBuildDate = datetime.datetime.utcnow(),
-    items = items
-)
-
-rss.write_xml(open("rss", "w"))
+main()
