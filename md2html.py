6a2ef84c70c021a5478f45a6feb660c55a80cf74
Thomas Levine <thomas.levine@gmail.com>
2012-05-16 08:32:08 -0400

    md2hmtml

--- md2html.py
+++ md2html.py
@@ -8,7 +8,7 @@ filename = sys.argv[1]
 os.system('''/usr/bin/vendor_perl/Markdown.pl "%s" | sed -e 's/<!--.*-->//' > tmp.html ''' % filename)
 html = fromstring(open('tmp.html').read())
 for p in html.cssselect('p'):
-    p.text = p.text.replace('\n', ' ')
+    html.replace(p, fromstring(tostring(p).replace('\n', ' ')))
 os.system('rm tmp.html')
 
 print tostring(html)[5:-6] #Remove the div tag
